2814a911fa7c1a7988596c14aa0f3f23b6584370
murilo <murilo@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-25 20:45:55 +0000

    [import] novo projeto de EFD - SPED
    git-svn-id: https://svn.autogeral.com.br/sped/EFDIcmsIpi@207 56544dd1-a790-4711-ae29-638a3112a704

---
+++ src/br/com/jcomputacao/sped/efd/icmsIpi/R0000.java
@@ -0,0 +1,56 @@
+
+package br.com.jcomputacao.sped.efd.icmsIpi;
+
+import br.com.jcomputacao.aristoteles.field.FieldArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDateTimeFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldIntegerMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.line.LineArchetype;
+
+/**
+ * 25/01/2013 17:14:28
+ * @author murilodemoraestuvani
+ */
+public class R0000 extends LineArchetype {
+    
+    public final static String REGISTRO = "REGISTRO";
+    public final static String COD_VER = "COD_VER";
+    public final static String COD_FIN = "COD_FIN";
+    public final static String DT_INI = "DT_INI";
+    public final static String DT_FIN = "DT_FIN";
+    public final static String NOME = "NOME";
+    public final static String CNPJ = "CNPJ";
+    public final static String UF = "UF";
+    public final static String IE = "IE";
+    public final static String COD_MUN = "COD_MUN";
+    public final static String IM = "IM";
+    public final static String SUFRAMA = "SUFRAMA";
+    public final static String IND_PERFIL = "IND_PERFIL";
+    public final static String IND_ATIV = "IND_ATIV";
+    
+    public R0000() {
+        setName("Registro 0000 - Aberturado do arquivo e identificacao da entidade");
+        setDelimiter("|");
+        addFieldArchetype(REGISTRO, new FieldDefaultArchetype("0000"));
+        addFieldArchetype(COD_VER, new FieldIntegerFixedLengthArchetype(3));
+        addFieldArchetype(COD_FIN, new FieldIntegerFixedLengthArchetype(1));
+        FieldArchetype fa = new FieldDateTimeFixedLengthArchetype("ddMMyyyy");
+        addFieldArchetype(DT_INI, fa);
+        addFieldArchetype(DT_FIN, fa);
+        addFieldArchetype(NOME, new FieldStringMaximumLengthArchetype(100));
+        addFieldArchetype(CNPJ, new FieldIntegerMaximumLengthArchetype(14));
+        addFieldArchetype(UF, new FieldStringFixedLengthArchetype(2));
+        addFieldArchetype(IE, new FieldStringMaximumLengthArchetype(14));
+        addFieldArchetype(COD_MUN, new FieldIntegerMaximumLengthArchetype(7));
+        addFieldArchetype(IM, new FieldStringMaximumLengthArchetype(30));
+        fa = new FieldStringMaximumLengthArchetype(9);
+        fa.setNullableRepresentation("");
+        addFieldArchetype(SUFRAMA, fa);
+        addFieldArchetype(IND_PERFIL, new FieldStringFixedLengthArchetype(2));
+        addFieldArchetype(IND_ATIV, new FieldIntegerFixedLengthArchetype(1));
+    }
+
+}
---
+++ test/br/com/jcomputacao/sped/efd/icmsIpi/R0000Test.java
@@ -0,0 +1,45 @@
+package br.com.jcomputacao.sped.efd.icmsIpi;
+
+import br.com.jcomputacao.aristoteles.line.LineModel;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Murilo
+ */
+public class R0000Test {
+    
+    @Test
+    public void testSomeMethod() throws ParseException {
+        R0000 reg = new R0000();
+        LineModel line = reg.createModel();
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+        
+        line.setFieldValue(R0000.COD_VER, 6);
+        line.setFieldValue(R0000.COD_FIN, 0);
+        
+        Date data = sdf.parse("01122012");
+        line.setFieldValue(R0000.DT_INI, data);
+        
+        data = sdf.parse("31122012");
+        line.setFieldValue(R0000.DT_FIN, data);
+        line.setFieldValue(R0000.NOME, "AUTO GERAL DE ITU LTDA");
+        line.setFieldValue(R0000.CNPJ, 5437537000137L);
+        line.setFieldValue(R0000.IE, "387.34.155.115");
+        line.setFieldValue(R0000.UF, "SP");
+        line.setFieldValue(R0000.COD_MUN, 3523909);
+        line.setFieldValue(R0000.IM, "12677");
+        line.setFieldValue(R0000.IND_PERFIL, "A");
+        line.setFieldValue(R0000.IND_ATIV, 1);
+
+        StringBuffer sb = line.getRepresentation();
+        String result = sb.toString();
+        String expected = "|0000|006|0|01122012|31122012|AUTO GERAL DE ITU LTDA|05437537000137||SP|387034155115|3523909|2623||A|1|";
+        assertEquals(expected, result);
+        System.out.print(result);
+    }
+}

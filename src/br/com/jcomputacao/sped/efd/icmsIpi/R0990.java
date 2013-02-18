0d5a6b23d71e0b172618a0fe4b6ab49977fb6813
murilo <murilo@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-18 18:10:59 +0000

    [update] SPED
    git-svn-id: https://svn.autogeral.com.br/sped/EFDIcmsIpi@383 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/icmsIpi/R0990.java
+++ src/br/com/jcomputacao/sped/efd/icmsIpi/R0990.java
@@ -1,5 +1,7 @@
 package br.com.jcomputacao.sped.efd.icmsIpi;
 
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldIntegerMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -9,4 +11,15 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
 public class R0990 extends LineArchetype {
 
     public static final String REGISTRO = "REG";
+    /**
+     * Quantidade total de linhas do arquivo digital.
+     */
+    public static final String QTD_LIN = "QTD_LIN";
+
+    public R0990() {
+        setName("Registro 0990 - Encerramento do blocl 0");
+        setDelimiter("|");
+        addFieldArchetype(REGISTRO, new FieldDefaultArchetype("0990"));
+        addFieldArchetype(QTD_LIN, new FieldIntegerMaximumLengthArchetype(20));
+    }    
 }

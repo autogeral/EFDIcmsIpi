517c8fd940561e00d7cc9f008c67196bd25d1532
murilo <murilo@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-19 15:35:56 +0000

    [update] SPED
    git-svn-id: https://svn.autogeral.com.br/sped/EFDIcmsIpi@389 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/icmsIpi/RH010.java
+++ src/br/com/jcomputacao/sped/efd/icmsIpi/RH010.java
@@ -1,5 +1,6 @@
 package br.com.jcomputacao.sped.efd.icmsIpi;
 
+import br.com.jcomputacao.aristoteles.field.FieldArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldDecimalMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
@@ -11,6 +12,7 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
  * 15/02/2013 09:56:55
+ *
  * @author Vinicius Diana
  */
 public class RH010 extends LineArchetype {
@@ -18,53 +20,53 @@ public class RH010 extends LineArchetype {
     /*
      * 1 - Texto fixo contendo "H010" 
      */
-   public static final String REGISTRO  = "REGISTRO ";
-   /*
+    public static final String REGISTRO = "REGISTRO ";
+    /*
      * 2 - Código do item (campo 02 do Registro 0200)  
      */
-   public static final String  COD_ITEM  = " COD_ITEM ";
-   /*
+    public static final String COD_ITEM = " COD_ITEM ";
+    /*
      * 3 -  Unidade do item  
      */
-   public static final String  UNID  = " UNID ";
-   /*
+    public static final String UNID = " UNID ";
+    /*
      * 4 -   Quantidade do item
      */
-   public static final String QTD  = "QTD";
-   /*
+    public static final String QTD = "QTD";
+    /*
      * 5 - Valor unitário do item 
      */
-   public static final String VL_UNIT  = "VL_UNIT ";
-   /*
+    public static final String VL_UNIT = "VL_UNIT ";
+    /*
      * 6 - Valor do item 
      */
-   public static final String  VL_ITEM  = " VL_ITEM";
-   /*
+    public static final String VL_ITEM = " VL_ITEM";
+    /*
      * 7 -  Indicador de propriedade/posse do item: 0- Item de propriedade do informante e em seu poder; 1- Item de propriedade do informante em posse de terceiros; 2- Item de propriedade de terceiros em posse do informante
 
      */
-   public static final String  IND_PROP  = " IND_PROP";
-   /*
+    public static final String IND_PROP = " IND_PROP";
+    /*
      * 8 - Código do participante (campo 02 do Registro 0150): - proprietário/possuidor que não seja o informante do arquivo 
      */
-   public static final String  COD_PART  = " COD_PART ";
-   /*
+    public static final String COD_PART = " COD_PART ";
+    /*
      * 9 - Descrição complementar. 
      */
-   public static final String TXT_COMPL = "TXT_COMPL ";
-   /*
+    public static final String TXT_COMPL = "TXT_COMPL ";
+    /*
      * 10 - Código da conta analítica contábil debitada/creditada 
      */
-   public static final String  COD_CTA   = " COD_CTA ";
+    public static final String COD_CTA = " COD_CTA ";
 
     public RH010() {
         setName("Registro H010 - inventario");
         setDelimiter("|");
-          FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
+        FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
         fw.setReplaceComa(false);
-        FormatWrapper fw1 = FormatFactory.getDecimalFormatWithPrecision(6);        
+        FormatWrapper fw1 = FormatFactory.getDecimalFormatWithPrecision(6);
         fw1.setReplaceComa(false);
-         FormatWrapper fw2 = FormatFactory.getDecimalFormatWithPrecision(3);        
+        FormatWrapper fw2 = FormatFactory.getDecimalFormatWithPrecision(3);
         fw2.setReplaceComa(false);
         //1
         addFieldArchetype(REGISTRO, new FieldDefaultArchetype("H010"));
@@ -73,32 +75,35 @@ public class RH010 extends LineArchetype {
         //3
         addFieldArchetype(UNID, new FieldStringMaximumLengthArchetype(6));
         //4
-        FieldDecimalMaximumLengthArchetype f1 = new FieldDecimalMaximumLengthArchetype(20,3);
+        FieldDecimalMaximumLengthArchetype f1 = new FieldDecimalMaximumLengthArchetype(20, 3);
         f1.setFormat(fw2);
         addFieldArchetype(QTD, f1);
         //5
-         FieldDecimalMaximumLengthArchetype f2 = new FieldDecimalMaximumLengthArchetype(20,6);
+        FieldDecimalMaximumLengthArchetype f2 = new FieldDecimalMaximumLengthArchetype(20, 6);
         f2.setFormat(fw1);
         addFieldArchetype(VL_UNIT, f2);
         //6
-         FieldDecimalMaximumLengthArchetype f3 = new FieldDecimalMaximumLengthArchetype(15,2);
+        FieldDecimalMaximumLengthArchetype f3 = new FieldDecimalMaximumLengthArchetype(15, 2);
         f3.setFormat(fw);
         addFieldArchetype(VL_ITEM, f3);
         //7
-        addFieldArchetype(IND_PROP,new  FieldStringFixedLengthArchetype(1));
-        //8
-        addFieldArchetype(COD_PART, new FieldStringMaximumLengthArchetype(60));
+        addFieldArchetype(IND_PROP, new FieldStringFixedLengthArchetype(1));
+        FieldArchetype fa = new FieldStringMaximumLengthArchetype(60);
+        fa.setNullableRepresentation("");
+        fa.setFullFillingNullable(false);
+        addFieldArchetype(COD_PART, fa);
         //9
-        addFieldArchetype(TXT_COMPL, new FieldStringMaximumLengthArchetype(255));
+        fa = new FieldStringMaximumLengthArchetype(255);
+        fa.setNullableRepresentation("");
+        fa.setFullFillingNullable(false);
+        addFieldArchetype(TXT_COMPL, fa);
         //10
-        addFieldArchetype(COD_CTA, new FieldStringMaximumLengthArchetype(500));
-        
-        
-        
+        fa = new FieldStringMaximumLengthArchetype(255);
+        fa.setNullableRepresentation("");
+        fa.setFullFillingNullable(false);
+        addFieldArchetype(COD_CTA, fa);
+
+
+
     }
-   
-   
-   
-   
-    
 }

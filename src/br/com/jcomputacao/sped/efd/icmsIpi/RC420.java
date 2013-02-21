4bcef8fc8d42f20a92c49f767b77810197f7b9fe
murilo <murilo@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-21 14:17:48 +0000

    [update] formatacao
    git-svn-id: https://svn.autogeral.com.br/sped/EFDIcmsIpi@408 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/icmsIpi/RC420.java
+++ src/br/com/jcomputacao/sped/efd/icmsIpi/RC420.java
@@ -11,53 +11,50 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
  * 18/02/2013 12:02:38
+ *
  * @author Thiago Balthazar
  */
-public class RC420 extends LineArchetype{
+public class RC420 extends LineArchetype {
 
-    public static final String REGISTRO = "REG"; 
     /*
      * Texto fixo contendo "C420"
      */
-    public static final String COD_TOT_PAR = "COD_TOT_PAR";
+    public static final String REGISTRO = "REG";
     /*
      * Código do totalizador, conforme Tabela 4.4.6
      */
-    public static final String VLR_ACUM_TOT = "VLR_ACUM_TOT";
+    public static final String COD_TOT_PAR = "COD_TOT_PAR";
     /*
      * Valor acumulado no totalizador, relativo à
-      respectiva Redução Z.
+     respectiva Redução Z.
+     */
+    public static final String VLR_ACUM_TOT = "VLR_ACUM_TOT";
+    /*
+     * Número do totalizador quando ocorrer mais de
+     uma situação com a mesma carga tributária efetiva.
      */
     public static final String NR_TOT = "NR_TOT";
     /*
-     * Número do totalizador quando ocorrer mais de
-       uma situação com a mesma carga tributária efetiva.
+     * Descrição da situação tributária relativa ao
+     totalizador parcial, quando houver mais de um
+     com a mesma carga tributária efetiva.
      */
     public static final String DESCR_NR_TOT = "DESCR_NR_TOT";
-    /*
-     * Descrição da situação tributária relativa ao
-       totalizador parcial, quando houver mais de um
-       com a mesma carga tributária efetiva.
-     */
 
     public RC420() {
-    
         setName("REGISTRO C420: REGISTRO DOS TOTALIZADORES PARCIAIS DA REDUCAO Z (COD 02 e 2D)");
         setDelimiter("|");
-        
+
         FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
         fw.setReplaceComa(false);
-     
-        FieldDecimalMaximumLengthArchetype decimal = new FieldDecimalMaximumLengthArchetype(15,2);
+
+        FieldDecimalMaximumLengthArchetype decimal = new FieldDecimalMaximumLengthArchetype(15, 2);
         decimal.setFormat(fw);
-        
+
         addFieldArchetype(REGISTRO, new FieldDefaultArchetype("C420"));
         addFieldArchetype(COD_TOT_PAR, new FieldStringMaximumLengthArchetype(7));
-        addFieldArchetype(VLR_ACUM_TOT, decimal);       
+        addFieldArchetype(VLR_ACUM_TOT, decimal);
         addFieldArchetype(NR_TOT, new FieldIntegerMaximumLengthArchetype(2));
         addFieldArchetype(DESCR_NR_TOT, new FieldStringMaximumLengthArchetype(255));
     }
-    
-    
-
 }

7ffd47d84eac0038c379d5bcbc235dd36578c233
douglasvieira <douglasvieira@56544dd1-a790-4711-ae29-638a3112a704>
2013-10-17 15:41:06 +0000

    [update]correcao comentario javadoc e espaçamento
    git-svn-id: https://svn.autogeral.com.br/sped/EFDIcmsIpi@436 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/icmsIpi/RC510.java
+++ src/br/com/jcomputacao/sped/efd/icmsIpi/RC510.java
@@ -5,242 +5,117 @@ import br.com.jcomputacao.aristoteles.format.FormatFactory;
 import br.com.jcomputacao.aristoteles.format.FormatType;
 import br.com.jcomputacao.aristoteles.format.FormatWrapper;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
-import java.lang.reflect.Field;
 
 /**
  * 18/02/2013 10:07:56
  * @author Vinicius Diana
  */
 public class RC510 extends LineArchetype {
-    /*
+    /**
      * 1 -  Texto fixo contendo "C510"
      */
     public static final String REGISTRO = "REGISTRO";
     
-    
-    
-    
-    
-    /*
+    /**
      * 2 -  Número sequencial do item no documento fiscal
      */
-     public static final String NUM_ITEM = "NUM_ITEM";
+    public static final String NUM_ITEM = "NUM_ITEM";
      
-     
-     
-     
-     
-     
-    /*
+    /**
      * 3 -   Código do item (campo 02 do Registro 0200)
      */
-     public static final String  COD_ITEM = " COD_ITEM";
+    public static final String  COD_ITEM = " COD_ITEM";
      
-     
-     
-     
-     
-     
-     
-    /*
+    /**
      * 4 -   Código de classificação do item de energia elétrica, conforme a Tabela 4.4.1
-
      */
-      public static final String  COD_CLASS  = " COD_CLASS ";
+    public static final String  COD_CLASS  = " COD_CLASS ";
       
-      
-      
-      
-      
-      
-    /*
+    /**
      * 5 -  Quantidade do item 
      */
-       public static final String  QTD = " QTD";
+    public static final String  QTD = " QTD";
        
-       
-       
-       
-       
-       
-       
-    /*
+    /**
      * 6 -   Unidade do item (Campo 02 do registro 0190) 
      */
-       public static final String  UNID  = " UNID ";
+    public static final String  UNID  = " UNID ";
        
-       
-       
-       
-       
-       
-    /*
+    /**
      * 7 -  Valor do item 
      */
-       public static final String  VL_ITEM  = "VL_ITEM "; 
+    public static final String  VL_ITEM  = "VL_ITEM "; 
        
-       
-       
-       
-       
-       
-    /*
+    /**
      * 8 -  Valor total do desconto
      */
-         public static final String  VL_DESC = " VL_DESC";
+    public static final String  VL_DESC = " VL_DESC";
          
-         
-         
-         
-         
-         
-         
-         
-    /*
+    /**
      * 9 -  Código da Situação Tributária, conforme a Tabela indicada no item 4.3.1
-
      */
-          public static final String  CST_ICMS  = " CST_ICMS ";
+    public static final String  CST_ICMS  = " CST_ICMS ";
           
-          
-          
-          
-          
-          
-    /*
+    /**
      * 10 -  Código Fiscal de Operação e Prestação 
      */
-           public static final String CFOP = "CFOP";
+    public static final String CFOP = "CFOP";
            
-           
-           
-           
-           
-    /*
+    /**
      * 11 -  Valor da base de cálculo do ICMS 
      */
-            public static final String VL_BC_ICMS = "VL_BC_ICMS";
+    public static final String VL_BC_ICMS = "VL_BC_ICMS";
             
-            
-            
-            
-            
-    /*
+    /**
      * 12 -  Alíquota do ICMS
      */
-             public static final String  ALIQ_ICMS = " ALIQ_ICMS";
+    public static final String  ALIQ_ICMS = " ALIQ_ICMS";
              
-             
-             
-             
-             
-    /*
+    /**
      * 13 -  Valor do ICMS creditado/debitado
      */
-              public static final String  VL_ICMS = " VL_ICMS";
+    public static final String  VL_ICMS = " VL_ICMS";
               
-              
-              
-              
-              
-              
-    /*
+    /**
      * 14 -  Valor da base de cálculo referente à substituição tributária
-
      */
-               public static final String VL_BC_ICMS_ST = "VL_BC_ICMS_ST";
+    public static final String VL_BC_ICMS_ST = "VL_BC_ICMS_ST";
                
-               
-               
-               
-               
-               
-               
-    /*
+    /**
      * 15 -  Alíquota do ICMS da substituição tributária na unidade da federação de destino
-
      */
-                public static final String ALIQ_ST = "ALIQ_ST";
+    public static final String ALIQ_ST = "ALIQ_ST";
                 
-                
-                
-                
-                
-                
-                
-                
-    /*
+    /**
      * 16 -  Valor do ICMS referente à substituição tributária
      */
-                 public static final String VL_ICMS_ST = "VL_ICMS_ST";
+    public static final String VL_ICMS_ST = "VL_ICMS_ST";
                  
-                 
-                 
-                 
-                 
-                 
-                 
-                 
-    /*
+    /**
      * 17 -  Indicador do tipo de receita: 0- Receita própria; 1- Receita de terceiros
-  
      */
-                  public static final String  IND_REC  = " IND_REC "; 
+    public static final String  IND_REC  = " IND_REC "; 
                   
-                  
-                  
-                  
-                  
-                  
-                  
-    /*
+    /**
      * 18 -  Texto fixo contendo "C510"
      */
-                   public static final String  COD_PART = " COD_PART";
+    public static final String  COD_PART = " COD_PART";
                    
-                   
-                   
-                   
-                   
-                   
-                   
-                   
-    /*
+    /**
      * 19 - Valor do PIS
      */
-     public static final String  VL_PIS  = " VL_PIS ";
-     
-     
-     
-     
-     
-     
-     
-         /*
+    public static final String  VL_PIS  = " VL_PIS ";
+    
+    /*
      * 20 - Valor da COFINS
      */           
-       public static final String  VL_COFINS = " VL_COFINS";   
-       
-       
-       
-       
-       
-       
-       
+    public static final String  VL_COFINS = " VL_COFINS";   
        
      /*
      * 21 - Código da conta analítica contábil debitada/creditada
-
      */   
-     public static final String   COD_CTA  = " COD_CTA ";
-     
-     
-     
-     
-     
-     
-     
-
+    public static final String   COD_CTA  = " COD_CTA ";
+    
     public RC510() {
         setName(" REGISTRO C510: ITENS DO DOCUMENTO NOTA FISCAL/CONTA ENERGIA ELÉTRICA (CÓDIGO 06), NOTA FISCAL/CONTA DE FORNECIMENTO D'ÁGUA CANALIZADA (CÓDIGO 29) E NOTA FISCAL/CONTA DE FORNECIMENTO DE GÁS (CÓDIGO 28). ");
         setDelimiter("|");
@@ -312,11 +187,5 @@ public class RC510 extends LineArchetype {
         addFieldArchetype(VL_COFINS, f11);
         //21
        addFieldArchetype(COD_CTA, new FieldStringMaximumLengthArchetype(255));
-       
     }
-     
-     
-    
-    
-
 }

e27f59f0850150a4238896a97ad4d699d8190699
douglasvieira <douglasvieira@56544dd1-a790-4711-ae29-638a3112a704>
2013-10-17 16:40:13 +0000

    [add] classe integracao sped
    git-svn-id: https://svn.autogeral.com.br/sped/EFDIcmsIpi@444 56544dd1-a790-4711-ae29-638a3112a704

---
+++ src/br/com/jcomputacao/sped/efd/icmsIpi/RC465.java
@@ -0,0 +1,30 @@
+package br.com.jcomputacao.sped.efd.icmsIpi;
+
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldIntegerMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.line.LineArchetype;
+
+/**
+ *
+ * @author Douglas
+ */
+public class RC465 extends LineArchetype {
+    
+    public static final String REGISTRO = "REG";
+    /**
+     * Digito verificador (DV) da chave do CF-e.
+     */
+    public static final String CHV_CFE = "CHV_CFE";
+    /**
+     * Campo deve ser maior que 0 (zero)
+     */
+    public static final String NUM_CCF = "NUM_CCF";
+
+    public RC465() {
+        setName("Registro C465 - Complemento do Cupom Fiscal eletronico por ECF - Cf-e - ECF(Codigo 60)");
+        setDelimiter("|");
+        addFieldArchetype(REGISTRO, new FieldDefaultArchetype("C465"));
+        addFieldArchetype(CHV_CFE, new FieldIntegerMaximumLengthArchetype(44));
+        addFieldArchetype(NUM_CCF, new FieldIntegerMaximumLengthArchetype(9));
+    }
+}
